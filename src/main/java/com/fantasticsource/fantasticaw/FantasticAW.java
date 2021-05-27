9563f0ceae8b68b62df18f47aa14d779524ca8b0
Kyle Koder <silvertallon@gmail.com>
2021-05-27 12:10:45 -0700

    1.12.2.000c; lib update

--- src/main/java/com/fantasticsource/fantasticaw/FantasticAW.java
+++ src/main/java/com/fantasticsource/fantasticaw/FantasticAW.java
@@ -16,12 +16,12 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = FantasticAW.MODID, name = FantasticAW.NAME, version = FantasticAW.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.044zzzz,);required-after:armourers_workshop@[1.12.2-0.51.0.659,)")
+@Mod(modid = FantasticAW.MODID, name = FantasticAW.NAME, version = FantasticAW.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.044zzzzg,);required-after:armourers_workshop@[1.12.2-0.51.0.659,)")
 public class FantasticAW
 {
     public static final String MODID = "fantasticaw";
     public static final String NAME = "Fantastic AW";
-    public static final String VERSION = "1.12.2.000b";
+    public static final String VERSION = "1.12.2.000c";
 
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent event)
